74756ca2605b4445ba8078702fc9717a2f0ff538
UnlegitSenpaii <39647545+UnlegitSenpaii@users.noreply.github.com>
2022-01-02 01:28:38 +0100

    initial release [0.1]

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# The Two of Us Window Resizer
- bonk
+
+# TToU Window Changer
+
+Hello.
+If you're here, it means you also don't like that the game "The Two of Us"  is starting in windowed mode as well as the fullscreen mode not being implemented correctly.
+
+It also uses your monitor's native resolution and not the predefined window resolution of the game.
+
+## Usage
+You can download the most recent release in the release section.
+When starting the app, you can choose between the following:
+
+ 0. Maximised Window
+ 1. Borderless Window
+ 2. [To-Do] Exclusive Fullscreen 
+
+Simply type the number of the mode you want when asked for it.
+
+#### Note
+Exclusive Fullscreen isn't implemented yet.
+-> So you don't click all over the place when having multiple monitors, which happens quite often.
---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+extern BOOL CALLBACK ParseWindow(HWND hwnd, LPARAM param);
+extern bool HandleUserInput();
+
+constexpr const char* targetWindow = "The Two of Us";
+static HWND targetHandle = nullptr;
+
+int main() {
+	//set our console title
+	SetConsoleTitleA("TToU Resolution Changer");
+
+	//search for the "the two of us" window
+	while (!targetHandle) {
+		std::cout << "Waiting for " << targetWindow << "..\r";
+		EnumWindows(ParseWindow, NULL);
+		Sleep(500);
+	}
+
+	//just to make sure
+	if (!targetHandle)
+		return 1;
+
+	//handle user input, retry if invalid input
+	while (!HandleUserInput()) {
+		system("cls");
+		Sleep(100);
+	}
+
+	system("timeout 3");
+}
+
+BOOL CALLBACK ParseWindow(HWND hwnd, LPARAM param) {
+
+	//get length of the title
+	const int size = GetWindowTextLengthA(hwnd) + 1;
+
+	//allocate memory to the buffer
+	const auto buffer = static_cast<PSTR>(VirtualAlloc(nullptr, static_cast<DWORD>(size), MEM_COMMIT, PAGE_READWRITE));
+
+	if (!buffer)
+		return FALSE;
+
+	GetWindowTextA(hwnd, buffer, size);
+
+	if (std::string(buffer, size).find(targetWindow) == std::string::npos) //check if window exists
+		return FALSE;
+
+	if (std::string(buffer, size).find("Steam") != std::string::npos) //ignore steam windows
+		return FALSE;
+
+	std::cout << "Found window: " << buffer << std::endl;
+
+	targetHandle = hwnd; //assign to global handle
+	return TRUE;
+}
+
+bool HandleUserInput() {
+	std::cout << "What mode do you want the game to run in?\n" << "\t[0] - Maximized Window\n" <<
+		"\t[1] - Borderless Window\n" << std::endl;
+
+	//get user input
+	std::string userChoise;
+	std::cin >> userChoise;
+
+	//check if user input only contains numbers
+	if (!std::all_of(userChoise.begin(), userChoise.end(), isdigit))
+		return false;
+
+	//this is not good practise at all! 
+	switch (std::stoi(userChoise)) {
+	case 0: {
+		if (!PostMessage(targetHandle, WM_SYSCOMMAND, SC_MAXIMIZE, 0))
+			std::cout << "Failed to send WM_SYSCOMMAND!\n" << std::endl;
+		return true;
+	}
+	case 1: {
+		RECT desktopResolution;
+		GetWindowRect(GetDesktopWindow(), &desktopResolution);//fuck it, we don't need no null check for that
+		SetWindowLongPtrW(targetHandle, GWL_STYLE, WS_POPUP | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX);
+		SetWindowPos(
+			targetHandle, nullptr, 0, 0, desktopResolution.right, desktopResolution.bottom,
+			SWP_FRAMECHANGED | SWP_NOZORDER
+		);
+		ShowWindow(targetHandle, SW_SHOW);
+		return true;
+	}
+	//todo: case 2:
+	default: {
+		return false;
+	}
+	}
+}
